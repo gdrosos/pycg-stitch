1f2fac7a420d8fa7d092f5427ec4c827b69a4417
gdrosos <t8180024@aueb.gr>
2022-06-01 21:04:13 +0300

    Add example test case

--- benchmark/micro/packages/dep1/dep1/dep1.py
+++ benchmark/micro/packages/dep1/dep1/dep1.py
@@ -1,6 +1,11 @@
 from trans_dep1.trans_dep1 import ClsPar
 from trans_dep2.trans_dep2 import smth
+import kkk
 
 class Cls(ClsPar):
     def dep_fn(self):
         smth()
+
+# check this case
+#unresolved
+kkk.a()
--- benchmark/micro/packages/root/root/root.py
+++ benchmark/micro/packages/root/root/root.py
@@ -1,4 +1,5 @@
 from dep1.dep1 import Cls
+from dep1.dep1 import K
 from dep2 import dep2
 
 class A(Cls):
