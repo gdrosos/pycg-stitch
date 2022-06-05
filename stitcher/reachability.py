62d259dfe5be9896cd152a9f4db1b95cef43d15e
gdrosos <t8180024@aueb.gr>
2022-06-05 14:19:51 +0300

    Use a dict to corellate an id with a node object

--- stitcher/reachability.py
+++ stitcher/reachability.py
@@ -11,6 +11,7 @@ class ReachabilityDetector:
         self.root_package,  self.root_version = coordinate.split(":")
         self.visited_nodes = set()
         self.metrics = {}
+        self.id_to_node={}
         
         self.graph = self._load_to_networkx(callgraph)
         self.reach_all_nodes()
@@ -28,7 +29,8 @@ class ReachabilityDetector:
     def _load_to_networkx(self, callgraph):
         graph = nx.DiGraph()
         for node in callgraph["nodes"]:
-            graph.add_node(node, URI=callgraph["nodes"][node]["URI"])
+            self.id_to_node[node]= Node(self.callgraph["nodes"][node]["URI"], loc=self.callgraph["nodes"][node]["metadata"]['LoC'])
+            graph.add_node(node, URI=callgraph["nodes"][node]["URI"], LoC= callgraph["nodes"][node]["metadata"]['LoC'])
         for edge in callgraph["edges"]:
             graph.add_edge(edge[0], edge[1])
         return graph
@@ -43,17 +45,21 @@ class ReachabilityDetector:
     def find_entrypoints(self):
         entrypoints=set()
         for item in self.callgraph["nodes"]:
-            node = Node(self.callgraph["nodes"][item]["URI"])
-            if node.get_product()== self.root_package:
+            if self.id_to_node[item].get_product()== self.root_package:
                 entrypoints.add(item)
         return [x for x in entrypoints if  self.graph.in_degree(x)==0]
 
+    def finc_unreachable_loc(self):
+        for id in self.visited_nodes:
+            node = self.id_to_node[id]
+            print(node.to_string(), node.get_modname(), node.get_callable(), node.loc)
     
     def extract_metrics(self):
         self.metrics["product"] = self.root_package
         self.metrics["version"] = self.root_version
         self.metrics["total_nodes"] = len(self.nodes)
         self.metrics["visited_nodes"] = len(self.visited_nodes)
+        self.finc_unreachable_loc()
         if self.metrics["total_nodes"]:
             print(1-(self.metrics["visited_nodes"]/self.metrics["total_nodes"]))
         pprint.pprint(self.metrics)
